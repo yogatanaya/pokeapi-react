16f6e8fc4e58c444a04df1784560535883978e8c
Yoga <yogat@iseka.services>
2021-09-20 08:21:39 +0800

    :sparkle: initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      perPage: 20,
+      pokemons: []
+    };
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  loadMore() {
+    let getUrl = fetch(`https://pokeapi.co/api/v2/pokemon?offset=${this.state.perPage + 20}&limit=20`);
+
+    getUrl.then(response => response.json())
+    .then(data => this.setState({
+      pokemons: data.results
+    }))
+  }
+
+  componentDidMount() {
+    let apiUrl = fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=' + this.state.perPage);
+    
+    apiUrl.then(response => response.json())
+    .then(data => 
+      this.setState({
+      pokemons: data.results
+    })
+    )
+  }
+
+  render() {
+    const { pokemons } = this.state;
+
+    return (<div className="container">
+      <h1 className="text-left font-weight-bold"><strong>Pokemon Database</strong></h1>
+
+      <div className="row">
+        {pokemons.map((pokemon, idx) => 
+          <div className="col-md-4">
+            <div className="card border-0 shadow shadow-md mt-2" style={{ 'borderRadius': '40px', height: '360px' }}>
+
+              <div className="text-center">
+              <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} className="img-fluid rounded-circle" 
+              style={{ width: '50%' }}
+              />
+              </div>
+     
+
+              <div className="card-body" key={idx}>
+                <h2 className="card-title text-center"><strong>{pokemon.name}</strong></h2>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
+
+
+      <div className="row justify-content-center p-4">
+        <button className="btn btn-md btn-primary"
+        onClick={() => this.loadMore()}
+        >Show More</button>
+      </div>
+
+    </div>)
+  }
 }
 
 export default App;
